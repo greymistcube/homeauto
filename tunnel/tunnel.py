b6eac27cbfd1cfcb35e306aff188edd001c4a0c7
Say Cheong <greymistcube@gmail.com>
2021-01-19 22:42:50 +0900

    absolute paths removed

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -7,7 +7,7 @@ from config import REMOTE, USER, KEY_PATH, REMOTE_PORT, LOCAL_PORT
 BOOT_WAIT_TIME = 60
 RECONNECT_WAIT_TIME = 60
 
-ssh_bin = "/usr/bin/ssh"
+ssh_bin = "ssh"
 ssh_args = [
     ssh_bin,
     "-i", f"{KEY_PATH}",
@@ -16,7 +16,7 @@ ssh_args = [
     f"{USER}@{REMOTE}",
 ]
 
-pushover_bin = "/usr/bin/pushover.py"
+pushover_bin = "pushover.py"
 pushover_msg = f"ssh connection to {REMOTE} has been dropped"
 pushover_args = [
     pushover_bin,
