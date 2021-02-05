1167eed9cb3d4c0b4efb564f55ef81e9d7d6558b
Say Cheong <greymistcube@gmail.com>
2021-02-05 17:52:02 +0900

    pathing fix

--- homeauto/ac_config.py
+++ homeauto/ac_config.py
@@ -1,6 +1,11 @@
-import json
+import os, json
 
-with open("ac_cred.json", "r") as file:
+# setting up paths
+MOD_PATH = os.path.realpath(__file__)
+DIR_PATH = os.path.dirname(MOD_PATH)
+CRED_PATH = os.path.join(DIR_PATH, "ac_cred.json")
+
+with open(CRED_PATH, "r") as file:
     cred = json.load(file)
 
 ON_PATTERN = cred["on_pattern"]
--- homeauto/hue_config.py
+++ homeauto/hue_config.py
@@ -1,6 +1,11 @@
-import json
+import os, json
 
-with open("hue_cred.json", "r") as file:
+# setting up paths
+MOD_PATH = os.path.realpath(__file__)
+DIR_PATH = os.path.dirname(MOD_PATH)
+CRED_PATH = os.path.join(DIR_PATH, "hue_cred.json")
+
+with open(CRED_PATH, "r") as file:
     cred = json.load(file)
 
 URL = cred["url"]
