dbfd5b3c91982277a9f5b85851f9f2d82779c4e0
Say Cheong <greymistcube@gmail.com>
2021-02-05 17:32:25 +0900

    additional sensative data moved to json

--- tunnel/config.py
+++ tunnel/config.py
@@ -1,8 +1,13 @@
-REMOTE: str # <remote_address>
-USER: str # <user>
-KEY_PATH: str # <key_path>
-REMOTE_PORT: int # <remote_port>
-LOCAL_PORT: int # <local_port>
+import json
+
+with open("config.json", "r") as file:
+    cred = json.load(file)
+
+REMOTE = cred["remote"]
+USER = cred["user"]
+KEY = cred["key"]
+REMOTE_PORT = cred["remote_port"]
+LOCAL_PORT = cred["local_port"]
 
 # wait times
 BOOT_WAIT_TIME = 60
